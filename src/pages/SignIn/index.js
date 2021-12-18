f77ae84f5d81e47d8a9da7addf87a1eb74cf95e2
Lucas Santos <lucas201273@gmail.com>
2021-12-18 12:43:03 -0300

    feat: add SignIn page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Container, Input,Form, Button } from "semantic-ui-react";
+import  GoogleButton from 'react-google-button'
+import './styles.css'
 
 export function SignIn() {
-  return <h1> Login Screen</h1>;
+  return (
+    <Container content>
+     <div className="content">
+       <h1> FireBase Auth</h1>
+       <Form>
+        <Input type="text" placeholder="Email"/>
+        <Input type="password" placeholder="Senha"/>
+        <Button content={'Log In'}/>
+       </Form>
+       <div className="google-button">
+         <GoogleButton style={ {
+           width: '100%'
+         }}/>
+       </div>
+       
+     </div>
+    </Container>
+  )
 }
